944c3e59f69ef72e3e2ce646f34a7de47090384f
Leven Rochana <levenrok@proton.me>
2026-02-20 14:40:38 +0530

    add basic arguments to the parser

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,20 @@
 use clap::Parser;
 
-#[derive(Parser, Debug)]
-#[command(version, about, long_about = None)]
-struct Args {}
+#[derive(Parser)]
+#[command(version, about)]
+struct Args {
+    #[arg(short, long, value_name = "name")]
+    create: Option<String>,
+
+    #[arg(short = 'b', long, value_name = "value", requires = "create")]
+    shebang: Option<String>,
+
+    #[arg(short, long)]
+    list: bool,
+
+    #[arg(short, long, value_name = "name")]
+    show: Option<String>,
+}
 
 #[tokio::main]
 async fn main() {
