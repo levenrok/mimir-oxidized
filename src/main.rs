fce760fa323eb094be6cc11a9e42c7984101df51
Leven Rochana <levenrok@proton.me>
2026-02-23 00:13:48 +0530

    add command to generate a autocompletion script

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use clap::Parser;
+use clap::{CommandFactory, Parser};
+use clap_complete::{Shell, generate};
 
 mod database;
 use database::Database;
@@ -6,22 +7,38 @@ use database::Database;
 #[derive(Parser)]
 #[command(version, about)]
 struct Args {
+    /// Create a new script
     #[arg(short, long, value_name = "name")]
     create: Option<String>,
 
+    /// Specify a shebang to the new script
     #[arg(short = 'b', long, value_name = "value", requires = "create")]
     shebang: Option<String>,
 
+    /// List all scripts
     #[arg(short, long)]
     list: bool,
 
+    /// Show contents of the specified script
     #[arg(short, long, value_name = "name")]
     show: Option<String>,
+
+    /// Generate autocompletion script
+    #[arg(long, value_name = "shell")]
+    generate: Option<Shell>,
 }
 
 #[tokio::main]
 async fn main() {
-    _ = Args::parse();
+    let mut cmd = Args::command();
+    let args = Args::parse();
+
+    let bin = env!("CARGO_BIN_NAME").to_string();
+
+    if let Some(generator) = args.generate {
+        generate(generator, &mut cmd, bin, &mut std::io::stdout());
+        return;
+    }
 
     let conn = Database::open_database("db.sqlite3").await;
 
