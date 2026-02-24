eab09d5eac0f995453780cb42008b05356baac9e
Leven Rochana <levenrok@proton.me>
2026-02-24 12:17:19 +0530

    update database migration for the scripts table

--- migrations/0001_create_scripts_table.sql
+++ migrations/0001_create_scripts_table.sql
@@ -1,5 +1,13 @@
-CREATE TABLE IF NOT EXISTS scripts(
+CREATE TABLE scripts(
     id INTEGER PRIMARY KEY,
-    name TEXT NOT NULL,
-    content TEXT
-);
+    name TEXT(1024) NOT NULL UNIQUE,
+    content TEXT,
+    shebang TEXT(255),
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP
+);-->statement breakpoint
+CREATE TRIGGER update_timestamp
+AFTER UPDATE ON scripts
+BEGIN
+    UPDATE scripts SET updated_at = CURRENT_TIMESTAMP WHERE id = OLD.id;
+END;
